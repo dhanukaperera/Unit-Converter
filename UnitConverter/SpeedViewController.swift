c3e8896cfb2f90d0ddd0d9c659e95d10d2d01100
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-08-18 11:36:42 +0530

    Call and display the conversions of mps, ftpsec, kmph

--- UnitConverter/SpeedViewController.swift
+++ UnitConverter/SpeedViewController.swift
@@ -10,6 +10,64 @@ import UIKit
 
 class SpeedViewController: UIViewController {
 
+    let speed = Speed()
+    
+    @IBOutlet weak var txtMps: UITextField!
+    @IBOutlet weak var txtFpm: UITextField!
+    @IBOutlet weak var txtKmph: UITextField!
+    @IBOutlet weak var txtMph: UITextField!
+    
+    
+    @IBAction func convertMpsToOther(_ sender: UITextField) {
+        var mps = Float32(txtMph.text!)
+        
+        if mps == nil {
+            mps = 0.0
+        }
+        
+        txtFpm.text = String(speed.convertMeterPerSecToFeetPerMin(mps: mps!))
+        txtKmph.text = String(speed.convertMeterPerSecToKmPerHour(mps: mps!))
+        txtMph.text = String(speed.convertMeterPerSecToMilesPerHour(mps: mps!))
+
+    }
+    
+    @IBAction func convertFpmToOther(_ sender: UITextField) {
+        var fpm = Float32(txtFpm.text!)
+        
+        if fpm == nil {
+            fpm = 0.0
+        }
+        
+        txtFpm.text = String(speed.convertFeetPerMinToMeterPerSec(fpm: fpm!))
+        txtKmph.text = String(speed.convertFeetPerMinToKmPerHour(fpm: fpm!))
+        txtMph.text = String(speed.convertFeetPerMinToMilesPerHour(fpm: fpm!))
+        
+    }
+    
+    
+    @IBAction func convertKphToOther(_ sender: UITextField) {
+        var kph = Float32(txtKmph.text!)
+        
+        if kph == nil {
+            kph = 0.0
+        }
+        
+        txtMph.text = String(speed.convertKmPerHourToMeterPerSec(kmps: kph!))
+        txtFpm.text = String(speed.convertKmPerHourToFeetPerMin(kmps: kph!))
+        txtMph.text = String(speed.convertKmPerHourToMilesPerHour(kmps: kph!))
+
+    }
+    
+    @IBAction func convertToMphToOther(_ sender: UITextField) {
+       var  mph = Float32(txtMph.text!)
+        
+        if mph == nil {
+            mph = 0.0
+        }
+        
+       // txtMph.text = String(speed.milesPerHourToMetersPerSec)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +79,4 @@ class SpeedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
