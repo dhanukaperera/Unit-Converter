54211e66908cf9e7c9990ffe7c832636ddb02a65
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-08-18 22:53:29 +0530

    refactor and clear code in Weights

--- UnitConverter/WeightViewController.swift
+++ UnitConverter/WeightViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  WeightViewController.swift
 //  UnitConverter
 //
 //  Created by Dhanuka Perera on 8/10/18.
@@ -21,63 +21,50 @@ class WeightViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    // Update the convertions in EditText Action
     @IBAction func convertGramToOtherUnits(_ sender: UITextField) {
-        var grams = Float32(txtGram.text!)
-        
-        if grams == nil {
-            grams = 0.0
+        if !(txtGram.text!.isEmpty) {
+            let grams = Float32(txtGram.text!)
+            txtKg.text = String(weights.convertGtoKg(g: grams!))
+            txtPound.text = String(weights.convertGtoPound(g: grams!))
+            txtOunce.text = String(weights.convertGtoOunce(g: grams!))
         }
-        
-        txtKg.text = String(weights.convertGtoKg(g: grams!))
-        txtPound.text = String(weights.convertGtoPound(g: grams!))
-        txtOunce.text = String(weights.convertGtoOunce(g: grams!))
     }
     
     @IBAction func convertKgToOtherUnits(_ sender: UITextField) {
-        var kg = Float32(txtKg.text!)
-        
-        if kg == nil {
-            kg = 0.00
+        if !(txtKg.text!.isEmpty) {
+            let kg = Float32(txtKg.text!)
+            txtGram.text = String(weights.convertKgToG(kg: kg!))
+            txtPound.text = String(weights.convertKgtoPounds(kg: kg!))
+            txtOunce.text = String(weights.convertKgToOunce(kg: kg!))
         }
-        
-        txtGram.text = String(weights.convertKgToG(kg: kg!))
-        txtPound.text = String(weights.convertKgtoPounds(kg: kg!))
-        txtOunce.text = String(weights.convertKgToOunce(kg: kg!))
-        
     }
     
     @IBAction func convertPoundToOtherUnits(_ sender: UITextField) {
-        var pound = Float32(txtPound.text!)
-        
-        if pound == nil {
-            pound = 0
+        if !(txtPound.text!.isEmpty) {
+            let pound = Float32(txtPound.text!)
+            txtGram.text = String(weights.convertPoundsToG(pounds: pound!))
+            txtKg.text = String(weights.convertPoundsToKg(pounds: pound!))
+            txtOunce.text = String(weights.convertPoundsToOunce(pounds: pound!))
         }
-        
-        txtGram.text = String(weights.convertPoundsToG(pounds: pound!))
-        txtKg.text = String(weights.convertPoundsToKg(pounds: pound!))
-        txtOunce.text = String(weights.convertPoundsToOunce(pounds: pound!))
     }
     
     @IBAction func convertOunceToOtherUnits(_ sender: UITextField) {
-        var ounce = Float32(txtOunce.text!)
-        
-        if ounce == nil {
-            ounce = 0.0
+        if !(txtOunce.text!.isEmpty) {
+            let ounce = Float32(txtOunce.text!)
+            txtGram.text = String(weights.convertOunceToG(ounce: ounce!))
+            txtKg.text = String(weights.convertOunceToKg(ounce: ounce!))
+            txtPound.text = String(weights.convertOunceToPounds(ounce: ounce!))
         }
-        
-        txtGram.text = String(weights.convertOunceToG(ounce: ounce!))
-        txtKg.text = String(weights.convertOunceToKg(ounce: ounce!))
-        txtPound.text = String(weights.convertOunceToPounds(ounce: ounce!))
     }
     
+    // Clear Text Fields
     @IBAction func clearGram(_ sender: UITextField) {
         txtGram.text = ""
     }
