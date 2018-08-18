9b25b44737fb5d5b865d3cc19ccc2c753d3bcb4e
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-08-18 10:45:43 +0530

    implant the methods to convert seed uint types

--- UnitConverter/Speed.swift
+++ UnitConverter/Speed.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+class Speed {
+    
+    // Meters per seconds convertions
+    func convertMeterPerSecToFeetPerMin(mps:Float32) -> Float32 {
+        return mps * 196.85
+    }
+    
+    func convertMeterPerSecToKmPerHour(mps:Float32) -> Float32 {
+        return mps * 3.6
+    }
+    
+    func convertMeterPerSecToMilesPerHour(mps:Float32) -> Float32 {
+        return mps * 2.23694
+    }
+    
+    // Feet per  min convertions
+    func convertFeetPerMinToMeterPerSec(fpm:Float32) -> Float32 {
+        return fpm * 0.00508
+    }
+    
+    func  convertFeetPerMinToKmPerHour(fpm:Float32) -> Float32 {
+        return fpm * 0.018288
+    }
+    
+    func convertFeetPerMinToMilesPerHour(fpm:Float32) -> Float32 {
+        return fpm * 0.0113636
+    }
+    
+    // Kilometer per hour converstions
+    func convertKmPerHourToMeterPerSec(kmps:Float32) -> Float32 {
+        return kmps * 1000
+    }
+    
+    func convertKmPerHourToFeetPerMin(kmps:Float32) -> Float32 {
+        return kmps * 196850
+    }
+    
+    func convertKmPerHourToMilesPerHour(kmps:Float32) -> Float32 {
+        return kmps * 2236.94
+    }
+    
+    // Miles per hour convertions
+    func milesPerHourToMetersPerSec(mph:Float32) -> Float32 {
+        return mph * 0.44704
+    }
+    
+    func milesPerHourToFeetPerMin(mph:Float32) -> Float32{
+        return mph * 88
+    }
+    
+    func milesPerHourToKmPerHour (mph:Float32) -> Float32 {
+        return mph * 1.60934
+    }
+    
+}
